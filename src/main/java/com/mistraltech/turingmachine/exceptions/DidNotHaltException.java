a4dd61988b4b2c42aa6735ee436d8ad96c784cb5
Richard Wilson <richard@mistraltech.co.uk>
2020-02-29 16:30:10 +0000

    Reword exception message

--- src/main/java/com/mistraltech/turingmachine/exceptions/DidNotHaltException.java
+++ src/main/java/com/mistraltech/turingmachine/exceptions/DidNotHaltException.java
@@ -3,6 +3,6 @@ package com.mistraltech.turingmachine.exceptions;
 public class DidNotHaltException extends RuntimeException {
 
   public DidNotHaltException(int iterations) {
-    super(String.format("Machine did not halt after %d steps", iterations));
+    super(String.format("Machine did not halt within %d steps", iterations));
   }
 }
